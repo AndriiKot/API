e5cdd837ff0fb024eea13199e0ab817eeceb6d78
AndriiKot <a_kocyuba@ukr.net>
2024-02-27 20:21:51 +0100

    test script main_ls

---
+++ DARK_MODE/main.js
@@ -0,0 +1,12 @@
+function box_size(){
+  let max_width = `${screen.width}`;
+  let max_height = `${screen.height}`;
+  let d = document.querySelectorAll('.box')
+  d.forEach (d => {
+  d.style.width = max_width / 1.8 +'px';
+  d.style.height = max_height / 4.2655 +'px';
+  });
+  d = document.querySelector('body')
+  d.style.gap = max_width / 6.4 + 'px';
+
+}

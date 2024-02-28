06be07e783b1196293e228633d1dc41325221d20
AndriiKot <a_kocyuba@ukr.net>
2024-02-28 13:54:52 +0100

    test babel and webpack

--- Open-meteo/app.js
+++ Open-meteo/app.js
@@ -10,7 +10,7 @@ const url = "https://api.open-meteo.com/v1/forecast";
 const responses = await fetchWeatherApi(url, params);
 
 // Helper function to form time ranges
-const range = (start: number, stop: number, step: number) =>
+const range = (start, stop, step) =>
 	Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
 
 // Process first location. Add a for-loop for multiple locations or weather models
@@ -23,8 +23,8 @@ const timezoneAbbreviation = response.timezoneAbbreviation();
 const latitude = response.latitude();
 const longitude = response.longitude();
 
-const hourly = response.hourly()!;
-const daily = response.daily()!;
+const hourly = response.hourly();
+const daily = response.daily();
 
 // Note: The order of weather variables in the URL query and the indices below need to match!
 const weatherData = {
@@ -33,13 +33,13 @@ const weatherData = {
 		time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
 			(t) => new Date((t + utcOffsetSeconds) * 1000)
 		),
-		temperature2m: hourly.variables(0)!.valuesArray()!,
+		temperature2m: hourly.variables(0).valuesArray(),
 	},
 	daily: {
 		time: range(Number(daily.time()), Number(daily.timeEnd()), daily.interval()).map(
 			(t) => new Date((t + utcOffsetSeconds) * 1000)
 		),
-		: daily.variables(0)!.valuesArray()!,
+		// daily.variables(0).valuesArray(),
 	},
 
 };
@@ -54,6 +54,6 @@ for (let i = 0; i < weatherData.hourly.time.length; i++) {
 for (let i = 0; i < weatherData.daily.time.length; i++) {
 	console.log(
 		weatherData.daily.time[i].toISOString(),
-		weatherData.daily.[i]
+		weatherData.daily[i]
 	);
 }

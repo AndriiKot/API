46a862c4dc641b914922c2fbbc7487dac305bbda
AndriiKot <a_kocyuba@ukr.net>
2024-02-29 11:45:28 +0100

    test click on an element tab

--- Open-meteo/main.js
+++ Open-meteo/main.js
@@ -2,10 +2,20 @@
 
 console.log(weatherData.current);
 
-const tabItems = document.querySelector(
-  ".tab-list__app-open-meteo"
+// TABS
+const tabItems = document.querySelector(".tab-list__app-open-meteo").children;
+const contentItems = document.querySelector(
+  ".content-list__app-open-meteo"
 ).children;
-const contentItems = document.querySelector(".content-list__app-open-meteo").children;
 
 console.dir(tabItems);
 console.dir(contentItems);
+
+const checkoutTabs = (item, index) => {
+  item.addEventListener("click", () => {
+    if (item.classList.contains("is-active")) return;
+    console.log(item);
+  });
+};
+
+Array.from(tabItems).forEach(checkoutTabs);

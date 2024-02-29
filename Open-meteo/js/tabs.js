ad7b6e52e80c18b944cd191db63030e44351b620
AndriiKot <a_kocyuba@ukr.net>
2024-02-29 12:33:15 +0100

    split main.js

---
+++ Open-meteo/js/tabs.js
@@ -0,0 +1,28 @@
+﻿const tabItems = Array.from(
+  document.querySelector(".tab-list__app-open-meteo").children
+);
+const contentItems = Array.from(
+  document.querySelector(".content-list__app-open-meteo").children
+);
+
+const clearActiveClass = (elements, className = "is-active") => {
+  elements.find((item) => item.classList.remove(`${className}`));
+};
+
+const setActiveClass = (elements, index, className = "is-active") => {
+  elements[index].classList.add(`${className}`);
+};
+
+const checkoutTabs = (item, index) => {
+  item.addEventListener("click", () => {
+    if (item.classList.contains("is-active")) return;
+
+    clearActiveClass(tabItems);
+    clearActiveClass(contentItems);
+
+    setActiveClass(tabItems, index);
+    setActiveClass(contentItems, index);
+  });
+};
+
+Array.from(tabItems).forEach(checkoutTabs);
--- Open-meteo/main.js
+++ Open-meteo/main.js
@@ -1,33 +1,5 @@
 ﻿import { weatherData } from "./weather_data.js";
+import "./js/tabs.js";
 
 console.log(weatherData.current);
 
-// TABS
-const tabItems = Array.from(
-  document.querySelector(".tab-list__app-open-meteo").children
-);
-const contentItems = Array.from(
-  document.querySelector(".content-list__app-open-meteo").children
-);
-
-const clearActiveClass = (elements, className = "is-active") => {
-  elements.find((item) => item.classList.remove(`${className}`));
-};
-
-const setActiveClass = (elements, index, className = "is-active") => {
-  elements[index].classList.add(`${className}`);
-};
-
-const checkoutTabs = (item, index) => {
-  item.addEventListener("click", () => {
-    if (item.classList.contains("is-active")) return;
-
-    clearActiveClass(tabItems);
-    clearActiveClass(contentItems);
-
-    setActiveClass(tabItems, index);
-    setActiveClass(contentItems, index);
-  });
-};
-
-Array.from(tabItems).forEach(checkoutTabs);

c747077d395dd9fd31d4640f0b53dc70e156b176
AndriiKot <a_kocyuba@ukr.net>
2024-02-29 11:36:10 +0100

    create new functional tabs

--- Open-meteo/main.js
+++ Open-meteo/main.js
@@ -1,3 +1,11 @@
-﻿import { weatherData } from './weather_data.js';
+﻿import { weatherData } from "./weather_data.js";
 
 console.log(weatherData.current);
+
+const tabItems = document.querySelector(
+  ".tab-list__app-open-meteo"
+).children;
+const contentItems = document.querySelector(".content-list__app-open-meteo").children;
+
+console.dir(tabItems);
+console.dir(contentItems);

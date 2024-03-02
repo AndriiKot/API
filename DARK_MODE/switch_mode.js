1402c93f68edcaa80d96a17064e9631cb701f465
AndriiKot <a_kocyuba@ukr.net>
2024-03-02 23:00:33 +0100

    version 0.0.1 Dark Mode and Light Mode

---
+++ DARK_MODE/switch_mode.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+const btn_switch_mode = document.querySelector(".btn__switch-mode");
+const boxes = document.querySelector(".box_wrapper_container-text").children;
+const body = document.body;
+
+btn_switch_mode.addEventListener(
+  "click",
+  (e, main = body, arr = Array.from(boxes)) => {
+    main.classList.toggle('dark-style')
+    
+    const btn = e.currentTarget;
+    btn.classList.toggle("light");
+
+
+    const btn_classes = btn.classList;
+    btn.innerText = btn_classes.contains("light") ? "dark" : "light";
+
+    arr.forEach((el) => {
+      el.classList.toggle("active");
+    });
+  }
+);

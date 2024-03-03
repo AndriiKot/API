d15078d62c1dc91f1ecca11633e8062ea47df234
AndriiKot <a_kocyuba@ukr.net>
2024-03-03 18:48:46 +0100

    version 1.0.0

--- DARK_MODE/switch_mode.js
+++ DARK_MODE/switch_mode.js
@@ -1,17 +1,21 @@
 ﻿"use strict";
 
 const btn_switch_mode = document.querySelector(".btn__switch-mode");
-const boxes = document.querySelector(".box_wrapper_container-text").children;
+const boxes = Array.from(document.querySelector(".box_wrapper_container-text").children);
 const body = document.body;
 
-
-const fn =  (btn = btn_switch_mode, main = body, arr = Array.from(boxes)) => {
+const dark_mode =  (e,btn = btn_switch_mode, main = body, arr = boxes) => {
   main.classList.toggle("dark-style");
-
   btn.classList.toggle("light");
 
   const btn_classes = btn.classList;
   btn.innerText = btn_classes.contains("light") ? "light" : "dark";
+
+    if (btn_classes.contains('light')) {
+      localStorage.setItem('dark-mode','dark-style')
+    } else {
+      localStorage.setItem('dark-mode','')
+    }
 
   arr.forEach((el) => {
     el.classList.toggle("active");
@@ -20,29 +24,8 @@ const fn =  (btn = btn_switch_mode, main = body, arr = Array.from(boxes)) => {
 
 
 if(localStorage.getItem('dark-mode')) {
-  fn()
+  dark_mode()
 }
 
-btn_switch_mode.addEventListener(
-  "click",
-  (e, main = body, arr = Array.from(boxes)) => {
-    main.classList.toggle("dark-style");
-
-    const btn = e.currentTarget;
-    btn.classList.toggle("light");
-
-    const btn_classes = btn.classList;
-    btn.innerText = btn_classes.contains("light") ? "light" : "dark";
-
-    if (btn_classes.contains('light')) {
-      localStorage.setItem('dark-mode','dark-style')
-    } else {
-      localStorage.setItem('dark-mode','')
-    }
-
-    arr.forEach((el) => {
-      el.classList.toggle("active");
-    });
-  }
-);
+btn_switch_mode.addEventListener("click", dark_mode);
 
